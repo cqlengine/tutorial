da5e6e05bc0c89ff830dc892a3588f1c7038c486
Jon Haddad <jon@jonhaddad.com>
2014-06-25 17:51:22 -0700

    added users and photos

--- bin/test.py
+++ bin/test.py
@@ -5,11 +5,19 @@ import nose
 import sys
 import os
 
+from cqlengine.management import sync_table
 # more stuff here i promise
 # makes the test runner work from top level
 sys.path.append(os.getcwd())
 
 from models.connection import connect
+
+
 connect()
+
+from models.users import User, Photo
+
+sync_table(User)
+sync_table(Photo)
 
 nose.run()
---
+++ models/users.py
@@ -0,0 +1,14 @@
+
+from cqlengine import *
+from uuid import uuid1
+
+
+class User(Model):
+    user_id = UUID(primary_key=True, default=uuid1)
+    name = Text()
+
+class Photo(Model):
+    # we're using a clustering key
+    user_id = UUID(primary_key=True)
+    photo_id = UUID(primary_key=True, default=uuid1)
+
