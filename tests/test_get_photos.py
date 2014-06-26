071b21f73f788d39e1f7f0c792cd1d360e1fa40d
Jon Haddad <jon@jonhaddad.com>
2014-06-25 19:14:29 -0700

    added basic test

---
+++ tests/test_get_photos.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from models.users import User, Photo
+
+
+class GetPhotosTest(TestCase):
+    def test_get_photos(self):
+        user = User.create(name="Jon Haddad")
+        photo = Photo.create(user_id=user.user_id,
+                             name="Profile Photo",
+                             url="https://pbs.twimg.com/profile_images/378800000375781556/2c9dedfd19613c88248d621cbe604857_400x400.jpeg")
+
+        photos = Photo.objects(user_id=user.user_id)
+
+        assert len(photos) == 1
+
+
